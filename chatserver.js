237fa93d70c548de76f7e038bd3a0f7fb35ab0d6
su <su@sus-MacBook-Air.local>
2024-09-25 00:14:37 -0400

    upd

--- chatserver.js
+++ chatserver.js
@@ -25,9 +25,9 @@ function formatDate(utctime) {
     const date = new Date(utctime * 1000);
     
     return date.toLocaleString(undefined, {
-        year: '2-digit',
-        month: '2-digit',
-        day: '2-digit',
+        // year: '2-digit',
+        // month: '2-digit',
+        // day: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
         hour12: false
