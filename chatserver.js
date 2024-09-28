09d41525b32939ef4bdbd8391d5f20d972bf5dd3
su <su@sus-MacBook-Air.local>
2024-09-28 02:05:05 -0400

    upd

--- chatserver.js
+++ chatserver.js
@@ -447,6 +447,7 @@ window.addEventListener('orientationchange', adjustContentHeight);
 
 messageInput.addEventListener('input', function () {
   this.style.height = 'auto';
+  const height = Math.max(this.scrollHeight, 100);
   this.style.height = this.scrollHeight + 'px';
   adjustContentHeight();
 }, false);
