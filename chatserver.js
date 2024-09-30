96a9bc889fce13609a52e662a07ed510be790667
su <su@sus-MacBook-Air.local>
2024-09-30 03:08:49 -0400

    upd

--- chatserver.js
+++ chatserver.js
@@ -57,7 +57,7 @@ function generateUsernameColor(username) {
     } 
     // Create a simple hash from the username
     let hash = 0;
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < username.length; i++) {
         const char = input.charCodeAt(i);
         hash = ((hash << 5) - hash) + char;
         hash = hash & hash; // Convert to 32-bit integer
